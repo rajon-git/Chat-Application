893a873ab5a1cc4f51b4b4264915350714351fcb
rajon-git <rajon.zhsust15@gmail.com>
2023-12-19 18:41:19 +0600

    auth api login,register complete

--- src/features/auth/authApi.js
+++ src/features/auth/authApi.js
@@ -1,7 +1,25 @@
 import { apiSlice } from "../api/apiSlice";
 
 export const authApi = apiSlice.injectEndpoints({
-    endpoints: (builder) => {
+    endpoints: (builder) => ({
         //endpoints here
-    }
+        //post request er jonno mutation
+        register: builder.mutation({
+            query: (data)=> ({
+                url: "/register",
+                method: "POST",
+                body: data,
+            }),
+        }),
+
+        login: builder.mutation({
+            query: (data)=> ({
+                url: "/login",
+                method: "POST",
+                body: data,
+            }),
+        }),
+    }),
 })
+
+export const {useLoginMutation, useRegisterMutation} = authApi;
